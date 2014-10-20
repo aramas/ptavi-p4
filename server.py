2549de20c7daf131ff77763207236d2c29e55ea8
leire soria indiano <lsoria@iota02.aulas.gsyc.es>
2014-10-20 10:58:54 +0200

    Escribimos la direccion Ip y el puerto que nos envía el cliente a nuestra máquina, el servidor. Pasamos también como parámetro el puerto que nos indican a la hora de ejecutar el servidor.

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ en UDP simple
 """
 
 import SocketServer
+import sys
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -14,17 +15,23 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
     """
 
     def handle(self):
-        # Escribe dirección y puerto del cliente (de tupla client_address)
+        #Mandamos el cliente una notificación que le informa
+        #de que hemos recibido su petición
         self.wfile.write("Hemos recibido tu peticion")
+        # Escribe dirección y puerto del cliente (de tupla client_address)
+        print self.client_address
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
-            print "El cliente nos manda " + line
             if not line:
                 break
+            else:
+                print "El cliente nos manda " + line
 
 if __name__ == "__main__":
+    #Extraer el dato dle puerto
+    PORT = int(sys.argv[1])
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
+    serv = SocketServer.UDPServer(("", PORT), EchoHandler)
     print "Lanzando servidor UDP de eco..."
     serv.serve_forever()
