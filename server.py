8225e2360d14c4439b34883341ed90ed1787a2a7
adrian ramas rayego <aramas@iota30.aulas.gsyc.es>
2014-10-27 13:17:57 +0100

    Ejercicio 8 SERVER

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
@@ -14,7 +15,25 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     Echo server class
     """
     dic_client = {}
+    datos_cliente = []
+
+    def register2file(self):
+        """
+        Metodo para copiar los datos de los usuarios a un archivo
+        """
+        fich = open("registered.txt", "w")
+        campos = "User" + '\t' + "IP" + '\t' + "Expires" + '\r\n'
+        fich.write(campos)
+        for Usuario in self.dic_client.keys():
+            fich.write(Usuario + '\t' + self.dic_client[Usuario][0] + '\t'
+                 + time.strftime('%Y-%m-%d %H:%M:%S', \
+                 time.gmtime(self.dic_client[Usuario][1]))
+                 + '\r\n')
+
     def handle(self):
+        """
+        Metodo para manejar los datos de los usuarios
+        """
         print self.client_address
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
@@ -23,14 +42,20 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 break
             lista_mensaje = mensaje.split(" ")
             Sip = lista_mensaje[1]
-            self.dic_client[Sip] = self.client_address[0]
+            Expire = lista_mensaje[4]
             if lista_mensaje[0] == "REGISTER":
+                ip = self.client_address[0]
+                hora_exp = int(time.time()) + int(Expire)
+                self.datos_cliente = [ip, hora_exp]
+                self.dic_client[Sip] = self.datos_cliente
                 self.wfile.write(lista_mensaje[2] + " 200 " + 'OK\r\n\r\n')
             # Borramos en caso de expires 0
-            if int(lista_mensaje[4]) == 0:
-                del self.dic_client[Sip]
-            print mensaje + " " + lista_mensaje[2] + '\r\n\r\n'
+            for Usuario in self.dic_client.keys():
+                if int(time.time()) >= self.dic_client[Usuario][1]:
+                    del self.dic_client[Usuario]
+            print mensaje + '\r\n\r\n'
             print self.dic_client
+            self.register2file()
 
 PUERTO = int(sys.argv[1])
 
