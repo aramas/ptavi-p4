57c9b0d39633c52c8842c38e931f77d856b2343e
leire soria indiano <lsoria@iota11.aulas.gsyc.es>
2014-10-27 16:16:48 +0100

    El servidor guarda en un fichero los registros sip

--- server.py
+++ server.py
@@ -7,14 +7,29 @@ en UDP simple
 
 import SocketServer
 import sys
+import time
 
 
 class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
     """
-    Echo server class
+    SIPRegisterHandler. Registro de dominios SIP
     """
+    def register2file(self):
+        """
+        Archivo de Registros en formato .txt
+        """
+        Archi = open('registered.txt', 'w')
+        for User in Dic_User.keys():
+            IP = Dic_User[User][0]
+            Time = time.strftime('%Y-%m-%d %H:%M:%S',
+                                 time.gmtime(Dic_User[User][1]))
+            Archi.write(User + '\t' + IP + '\t' + str(Time) + '\r\n')
+        Archi.close()
 
     def handle(self):
+        """
+        Manejador de registros SIP
+        """
         print "El cliente nos manda:"
         while 1:
             Line = self.rfile.read()
@@ -23,13 +38,16 @@ class SIPRegisterHandler(SocketServer.DatagramRequestHandler):
                 break
             else:
                 List = Line.split(' ')
-                Direccion_SIP = List[1].split(':')[1]
-                Dic_User[Direccion_SIP] = self.client_address[0]
+                Direc_SIP = List[1].split(':')[1]
+                IP = self.client_address[0]
+                Expires = time.time() + int(List[3])
+                Dic_User[Direc_SIP] = [IP, Expires]
+                self.register2file()
+                for User in Dic_User.keys():
+                    if time.time() >= Dic_User[User][1]:
+                        del Dic_User[User]
+                        self.register2file()
                 self.wfile.write("SIP/1.0 200 OK\r\n\r\n")
-                Expires = int(List[3])
-                if Expires == 0:
-                    del Dic_User[Direccion_SIP]
-                print "Los ususarios son:" + '\r\n' + str(Dic_User)
 
 if __name__ == "__main__":
     Dic_User = {}
